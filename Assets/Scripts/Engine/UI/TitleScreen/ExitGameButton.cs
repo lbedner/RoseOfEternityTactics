9a2e0a5f340341a9c955c6713cfba4662291bd43
Leonard Bedner <LeonardBedner@iheartmedia.com>
2017-05-24 20:53:57 -0400

    Exit Button On Title Screen Is Now Activated And Exits

---
+++ Assets/Scripts/Engine/UI/TitleScreen/ExitGameButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ExitGameButton : MonoBehaviour, IPointerClickHandler {
+
+	/// <summary>
+	/// Exits the game when the button is clicked.
+	/// </summary>
+	/// <param name="eventData">Event data.</param>
+	public void OnPointerClick (PointerEventData eventData)
+	{
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit ();
+		#endif
+	}
+}
